157e3fff7987d0711a7b6f9ba4417ab298e6caf7
tomcharo <shota.a_2807@softbank.ne.jp>
2023-10-14 00:28:08 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,13 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :image, :name, :info, :price, presence: true
+  validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width numbers" }
+  validates :price, numericality: { in: 300..9999999, message: "is out of setting range" }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_schedule_id, numericality: { other_than: 1, message: "can't be blank" }
 end
