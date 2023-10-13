ccd3447624bf9c21fbd0a4ab2467d82e3d593b3c
tomcharo <shota.a_2807@softbank.ne.jp>
2023-10-13 20:28:42 +0900

    ActiveHashの導入、モデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :delivery_schedule
+
   belongs_to :user
   has_one_attached :image
 end
