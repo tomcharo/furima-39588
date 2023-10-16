1e8433a6bd476e72c53dbcf4ea1516cab421501c
tomcharo <shota.a_2807@softbank.ne.jp>
2023-10-17 04:11:44 +0900

    Formオブジェクトの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,24 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
-    
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(@order_address.item_id)
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,15 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :city, :address, :building, :phone_number, :prefecture_id, :user_id, :item_id
+
+  validates :postal_code, :city, :address, :phone_number, :prefecture_id, :user_id, :item_id, presence: true
+  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :phone_number, numericality: { only_integer: true, message: "is invalid. Input half-width numbers" }
+  validates :phone_number, length: { in: 10..11, message: "is out of setting range" }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    DeliveryAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
